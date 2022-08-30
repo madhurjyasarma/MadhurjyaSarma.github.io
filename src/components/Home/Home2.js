810687217e7414d6099bff320811274a7a5e2762
Madhurjya Sarma <77984764+madhurjyasarma@users.noreply.github.com>
2022-08-31 03:37:03 +0530

    Home modified

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,22 +24,22 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> JAVA, Javascript and Python. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
                 <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                also {" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  Data Structures and Algorithms.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+                I'm currently learning Backend Technologies like
+              <b className="purple"> SpringBoot, Node.js</b> and also
               <i>
                 <b className="purple">
                   {" "}
@@ -48,7 +48,7 @@ function Home2() {
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> React.js.</b>
               </i>
             </p>
           </Col>
